d2237bc104190117a01b5d358ef4a18bc14e6e07
onzecki <git@onz.ee>
2024-11-29 05:58:39 +0100

    feat: add basic components

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,9 @@
+use html::P;
 use leptos::leptos_dom::ev::SubmitEvent;
 use leptos::*;
 use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::to_value;
+use svg::view;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -10,25 +12,122 @@ extern "C" {
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 }
 
+#[derive(Copy, Clone)]
+struct Preset(WriteSignal<String>);
+
 #[component]
 pub fn History() -> impl IntoView {
     view! {
-        <main>
+        <main id="historyMain">
+            <div id="historyTitle">
+                <h1>History</h1>
+            </div>
+            <div class="historyItem">
+                <div class="usedPreset">
+                    <div class="icon">                    
+                        <svg width="24px" height="24px" stroke-width="2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M20 10C20 14.4183 12 22 12 22C12 22 4 14.4183 4 10C4 5.58172 7.58172 2 12 2C16.4183 2 20 5.58172 20 10Z"  stroke-width="2"></path><path d="M12 11C12.5523 11 13 10.5523 13 10C13 9.44772 12.5523 9 12 9C11.4477 9 11 9.44772 11 10C11 10.5523 11.4477 11 12 11Z" fill="#000000"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                    </div>
+                    <div class="text">
+                        <h2>Table</h2>
+                    </div>
+                </div>
+                <div class="launchedTimestamp">
+                    <div class="icon">
+                        <svg width="24px" height="24px" stroke-width="2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M12 6L12 12L18 12"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                    </div>
+                    <div class="text">
+                        <div class="date"><p>2024-11-29</p></div>
+                        <div class="time"><p>05:47:19</p></div>
+                    </div>
+                </div>
+                <div class="totalLength">
+                    <div class="icon">
+                        <svg width="24px" height="24px" stroke-width="2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M9 2L15 2"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 10L12 14"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 22C16.4183 22 20 18.4183 20 14C20 9.58172 16.4183 6 12 6C7.58172 6 4 9.58172 4 14C4 18.4183 7.58172 22 12 22Z"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                    </div>
+                    <div class="text">
+                        <div class="time"><p>01:35:24</p></div>
+                    </div>    
+                </div>
+            </div>
         </main>
     }
 }
 #[component]
 pub fn New() -> impl IntoView {
+    let (playing, set_playing) = create_signal(false);
     view! {
-        <main>
-            <button>Create POST req</button>
+        <main id="addMain">
+            <div>
+                <h1>New job!</h1>
+            </div>
+            <div>
+                <h2>Current status:</h2>
+                <h3>{move || if playing.get() {
+                        "Running"
+                    }
+                    else {
+                        "Paused"
+                    }
+                    }
+                </h3>
+            </div>
+            <div></div>
+            <div></div>
+            <div>
+            <button id="play" on:click=move |_| {
+                set_playing.set(!playing.get());
+            }>
+            {move || if playing.get() {
+                view! { <Pause /> }
+            } else {
+                view! { <Play /> }
+            }}  
+            </button>
+            </div>
+            <div></div>
         </main>
     }
 }
 #[component]
+pub fn Play() -> impl IntoView {
+    view!{
+        <svg width="24px" height="24px" stroke-width="1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.90588 4.53682C6.50592 4.2998 6 4.58808 6 5.05299V18.947C6 19.4119 6.50592 19.7002 6.90588 19.4632L18.629 12.5162C19.0211 12.2838 19.0211 11.7162 18.629 11.4838L6.90588 4.53682Z" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+    }
+}
+#[component]
+pub fn Pause() -> impl IntoView {
+    view!{
+        <svg width="24px" height="24px" stroke-width="2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M6 18.4V5.6C6 5.26863 6.26863 5 6.6 5H9.4C9.73137 5 10 5.26863 10 5.6V18.4C10 18.7314 9.73137 19 9.4 19H6.6C6.26863 19 6 18.7314 6 18.4Z"  stroke-width="2"></path><path d="M14 18.4V5.6C14 5.26863 14.2686 5 14.6 5H17.4C17.7314 5 18 5.26863 18 5.6V18.4C18 18.7314 17.7314 19 17.4 19H14.6C14.2686 19 14 18.7314 14 18.4Z"  stroke-width="2"></path></svg>
+    }
+}
+
+
+#[component]
 pub fn Presets() -> impl IntoView {
     view! {
-        <main>
+        <main id="presetsMain">
+            <div id="presetTitle">
+                <h1>Presets</h1>
+            </div>
+
+            <div class="preset">
+                <div class="presetTop">
+                    <svg width="24px" height="24px" stroke-width="2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M20 10C20 14.4183 12 22 12 22C12 22 4 14.4183 4 10C4 5.58172 7.58172 2 12 2C16.4183 2 20 5.58172 20 10Z"  stroke-width="2"></path><path d="M12 11C12.5523 11 13 10.5523 13 10C13 9.44772 12.5523 9 12 9C11.4477 9 11 9.44772 11 10C11 10.5523 11.4477 11 12 11Z" fill="#000000"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                    <h2>Table</h2>
+                </div>
+                <div class="presetBottom">
+                    <img src="public/table.png"></img>
+                </div>
+            </div>
+            <div class="preset">
+                <div class="presetTop">
+                    <svg width="24px" height="24px" stroke-width="2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M20 10C20 14.4183 12 22 12 22C12 22 4 14.4183 4 10C4 5.58172 7.58172 2 12 2C16.4183 2 20 5.58172 20 10Z"  stroke-width="2"></path><path d="M12 11C12.5523 11 13 10.5523 13 10C13 9.44772 12.5523 9 12 9C11.4477 9 11 9.44772 11 10C11 10.5523 11.4477 11 12 11Z" fill="#000000"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                    <h2>Whiteboard</h2>
+                </div>
+                <div class="presetBottom">
+                    <img src="public/whiteboard.png"></img>
+                </div>
+            </div>
         </main>
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,11 @@ fn main() {
     mount_to_body(|| {
         view! {
             <Router>
-
-            <main>
                 <Routes>
                     <Route path="/presets" view=Presets/>
                     <Route path="/" view=New/>
                     <Route path="/history" view=History/>
                 </Routes>
-            </main>
             <Nav/>
         </Router>
         }
@@ -26,9 +23,10 @@ fn main() {
 pub fn Nav() -> impl IntoView {
     view!{
         <nav>
-            <a href="/presets"><svg stroke-width="1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 6L20 6" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M4 6.01L4.01 5.99889"  stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M4 12.01L4.01 11.9989"  stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M4 18.01L4.01 17.9989"  stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M8 12L20 12"  stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M8 18L20 18"  stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-            <a href="/"><svg stroke-width="1" viewBox="0 0 24 24"   xmlns="http://www.w3.org/2000/svg"><path d="M6 12H12M18 12H12M12 12V6M12 12V18" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-            <a href="/history"><svg viewBox="0 0 24 24" fill="none" stroke-width="1" xmlns="http://www.w3.org/2000/svg"><path d="M12 6L12 12L18 12" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M21.8883 10.5C21.1645 5.68874 17.013 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C16.1006 22 19.6248 19.5318 21.1679 16" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M17 16H21.4C21.7314 16 22 16.2686 22 16.6V21" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
+            <a id="presetsNav" href="/presets"><svg stroke-width="1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 6L20 6" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M4 6.01L4.01 5.99889"  stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M4 12.01L4.01 11.9989"  stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M4 18.01L4.01 17.9989"  stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M8 12L20 12"  stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M8 18L20 18"  stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
+            <a id="addNav" href="/"><svg stroke-width="1" viewBox="0 0 24 24"   xmlns="http://www.w3.org/2000/svg"><path d="M6 12H12M18 12H12M12 12V6M12 12V18" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
+            <a id="historyNav" href="/history"><svg viewBox="0 0 24 24" fill="none" stroke-width="1" xmlns="http://www.w3.org/2000/svg"><path d="M12 6L12 12L18 12" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M21.8883 10.5C21.1645 5.68874 17.013 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C16.1006 22 19.6248 19.5318 21.1679 16" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M17 16H21.4C21.7314 16 22 16.2686 22 16.6V21" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
         </nav>
     }
 }
+
